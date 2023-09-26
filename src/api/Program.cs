7055df94448c9a1d4e8bfaf5aa7ef7b48f9bc110
Tobias Kristensen <tobiaskristensen12@gmail.com>
2023-09-26 18:30:30 +0200

    Fix: Resolve merge conflicts
    temp commit
    
    temp commit 2

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,22 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
+using api.persistence;
+using Microsoft.EntityFrameworkCore;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-// }
-
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
